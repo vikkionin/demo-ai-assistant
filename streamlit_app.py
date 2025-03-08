9a57fd4fa502963ae2574c51d2e05a5d6050813b
Thiago Teixeira <thiago.teixeira@snowflake.com>
2025-03-08 14:38:42 -0800

    Add links at bottom of responses.

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,6 +70,7 @@ INSTRUCTIONS = textwrap.dedent("""
     - Respond with markdown.
     - Avoid experimental and private APIs.
     - Don't say things like "according to the provided context".
+    - When available, include links with related URLs at the bottom.
     - Streamlit is a product of Snowflake.
 """)
 
