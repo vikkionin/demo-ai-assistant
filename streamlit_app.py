4a2f2209b3717e67b6c44d4c8b84f42da56ea4a2
Thiago Teixeira <thiago.teixeira@snowflake.com>
2025-09-04 15:16:08 -0300

    Improve app a bunch: prettier homescreen, suggestions, custom theme, etc.

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from htbuilder.units import rem
+from htbuilder import div, styles
 from collections import namedtuple
 from concurrent.futures import ThreadPoolExecutor
 import datetime
@@ -23,13 +25,13 @@ from snowflake.core import Root
 from snowflake.cortex import complete
 
 
-st.set_page_config(page_title="Streamlit AI assistant", page_icon="🤖")
+st.set_page_config(page_title="Streamlit AI assistant", page_icon="✨")
 
 # -----------------------------------------------------------------------------
 # Set things up.
 
 
-@st.cache_resource(ttl="12h")
+@st.cache_resource(ttl="5m")
 def get_session():
     return st.connection("snowflake").session()
 
@@ -63,21 +65,21 @@ DEBUG_MODE = st.query_params.get("debug", "false").lower() == "true"
 
 INSTRUCTIONS = textwrap.dedent("""
     - You are a helpful AI chat assistant focused on answering quesions about
-      Streamlit, Streamlit Community Cloud, and general Python.
+      Streamlit, Streamlit Community Cloud, Snowflake, and general Python.
     - You will be given extra information provided inside tags like this
       <foo></foo>.
     - Use context and history to provide a coherent answer.
-    - Use markdown such as headers (starting with ###), code blocks, bullet
-      points, 3-space indentation for sub bullets, and backticks for inline
-      code and markdown features like icon names.
+    - Use markdown such as headers (starting with ##), code blocks, bullet
+      points, indentation for sub bullets, and backticks for inline code.
+    - Don't start the response with a markdown header.
     - Assume the user is a newbie.
-    - Write paragraphs of explanation, as if you're writing documentation.
-    - Offer alternatives where they exist.
+    - Be brief, but clear. If needed, you can write paragraphs of text, like
+      a documentation website.
+    - Avoid experimental and private APIs.
+    - Offer alternatives within the Streamlit and Snowflake universe.
     - Provide examples.
     - Include related links throughout the text and at the bottom.
-    - Avoid experimental and private APIs.
     - Don't say things like "according to the provided context".
-    - If you don't know, just say "I don't know the answer to that question."
     - Streamlit is a product of Snowflake.
 """)
 
@@ -260,72 +262,143 @@ def show_feedback_controls(message_index):
 
     with st.popover("How did I do?"):
         with st.form(key=f"feedback-{message_index}", border=False):
-            st.markdown(":small[Rating]")
-            rating = st.feedback("stars")
+            with st.container(gap=None):
+                st.markdown(":small[Rating]")
+                rating = st.feedback(options="stars")
 
-            details = st.text_area("More information")
+            details = st.text_area("More information (optional)")
 
             if st.checkbox("Include chat history with my feedback", True):
                 relevant_history = st.session_state.messages[:message_index]
             else:
                 relevant_history = []
 
-            st.form_submit_button("Send feedback")
+            ""  # Add some space
 
-            # TODO: Send feedback and history.
-            st.caption("PS: This is not connected to anything yet!")
+            if st.form_submit_button("Send feedback"):
+                # TODO: Submit feedback here!
+                pass
+
+
+@st.dialog("Legal disclaimer")
+def show_disclaimer_dialog():
+    st.caption("""
+            This AI chatbot is powered by Snowflake and public Streamlit
+            information. Answers may be inaccurate, inefficient, or biased.
+            Any use or decisions based on such answers should include reasonable
+            practices including human oversight to ensure they are safe,
+            accurate, and suitable for your intended purpose. Streamlit is not
+            liable for any actions, losses, or damages resulting from the use
+            of the chatbot. Do not enter any private, sensitive, personal, or
+            regulated data. By using this chatbot, you acknowledge and agree
+            that input you provide and answers you receive (collectively,
+            “Content”) may be used by Snowflake to provide, maintain, develop,
+            and improve their respective offerings. For more
+            information on how Snowflake may use your Content, see
+            https://streamlit.io/terms-of-service.
+        """)
 
 
 # -----------------------------------------------------------------------------
 # Draw the UI.
 
-cols = st.columns([3, 1], vertical_alignment="bottom")
 
-with cols[0]:
-    st.title("Streamlit AI assistant", anchor=False)
+st.html(div(style=styles(font_size=rem(5), line_height=1))["❉"])
 
-with cols[1]:
-    clear_conversation = st.button(
-        "Restart",
-        icon=":material/refresh:",
-        use_container_width=True,
+title_row = st.container(
+    horizontal=True,
+    vertical_alignment="bottom",
+)
+
+with title_row:
+    st.title(
+        # ":material/cognition_2: Streamlit AI assistant", anchor=False, width="stretch"
+        "Streamlit AI assistant",
+        anchor=False,
+        width="stretch",
     )
 
-if clear_conversation or "messages" not in st.session_state:
+SUGGESTIONS = {
+    ":blue[:material/local_library:] What is Streamlit?": (
+        """What is Streamlit, what is it great at, and what can I do with it?
+        """
+    ),
+    ":violet[:material/sprint:] Build an app with latest features": (
+        """Build an app showing 3 of the coolest Streamlit features of the
+        last 6 months"""
+    ),
+    ":red[:material/database:] Help me understand session state": (
+        """Help me understand session state. What is it for?
+        What are gotchas? What are alternatives?"""
+    ),
+    ":green[:material/multiline_chart:] How do I make an interactive chart?": (
+        """How do I make a chart where, when I click, another chart updates?
+        Show me examples with Altair or Plotly."""
+    ),
+}
+
+user_just_asked_initial_question = (
+    "initial_question" in st.session_state and st.session_state.initial_question
+)
+
+user_just_clicked_suggestion = (
+    "selected_suggestion" in st.session_state and st.session_state.selected_suggestion
+)
+
+user_first_interaction = (
+    user_just_asked_initial_question or user_just_clicked_suggestion
+)
+
+has_message_history = (
+    "messages" in st.session_state and len(st.session_state.messages) > 0
+)
+
+# Show a different UI when the user hasn't asked a question yet.
+if not user_first_interaction and not has_message_history:
     st.session_state.messages = []
+
+    with st.container():
+        st.chat_input("Ask a question...", key="initial_question")
+
+        selected_suggestion = st.pills(
+            label="Examples",
+            label_visibility="collapsed",
+            options=SUGGESTIONS.keys(),
+            key="selected_suggestion",
+        )
+
+    st.button(
+        "&nbsp;:small[:gray[:material/balance: Legal disclaimer]]",
+        type="tertiary",
+        on_click=show_disclaimer_dialog,
+    )
+
+    st.stop()
+
+# Show chat input at the bottom when a question has been asked.
+user_message = st.chat_input("Ask a follow-up...")
+
+if not user_message:
+    if user_just_asked_initial_question:
+        user_message = st.session_state.initial_question
+    if user_just_clicked_suggestion:
+        user_message = SUGGESTIONS[st.session_state.selected_suggestion]
+
+with title_row:
+
+    def clear_conversation():
+        st.session_state.messages = []
+        st.session_state.initial_question = None
+        st.session_state.selected_suggestion = None
+
+    st.button(
+        "Restart",
+        icon=":material/refresh:",
+        on_click=clear_conversation,
+    )
 
 if "prev_question_timestamp" not in st.session_state:
     st.session_state.prev_question_timestamp = datetime.datetime.fromtimestamp(0)
-
-with st.expander(
-    "This is an AI chatbot, so it may hallucinate. Expand to see legal disclaimer."
-):
-    st.write("""
-        This AI chatbot is powered by Snowflake and public Streamlit
-        information. Answers may be inaccurate, inefficient, or biased.
-        Any use or decisions based on such answers should include reasonable
-        practices including human oversight to ensure they are safe,
-        accurate, and suitable for your intended purpose. Streamlit is not
-        liable for any actions, losses, or damages resulting from the use
-        of the chatbot. Do not enter any private, sensitive, personal, or
-        regulated data. By using this chatbot, you acknowledge and agree
-        that input you provide and answers you receive (collectively,
-        “Content”) may be used by Snowflake to provide, maintain, develop,
-        and improve their respective offerings. For more
-        information on how Snowflake may use your Content, see
-        https://streamlit.io/terms-of-service.
-    """)
-
-st.info(
-    ":material/info: "
-    "This app uses "
-    f"[Snowflake Cortex]({CORTEX_URL}) "
-    f"and is [fully open source]({GITHUB_URL})! "
-)
-
-# Show a fake question from the assistant to get the user started.
-with st.chat_message("assistant"):
-    st.markdown("Hello, how may I help you?")
 
 # Display chat messages from history as speech bubbles.
 for i, message in enumerate(st.session_state.messages):
@@ -338,16 +411,16 @@ for i, message in enumerate(st.session_state.messages):
         if message["role"] == "assistant":
             show_feedback_controls(i)
 
-if question := st.chat_input("Ask a question..."):
+if user_message:
     # When the user posts a message...
 
     # Streamlit's Markdown engine interprets "$" as LaTeX code (used to
     # display math). The line below fixes it.
-    question = question.replace("$", r"\$")
+    user_message = user_message.replace("$", r"\$")
 
     # Display message as a speech bubble.
     with st.chat_message("user"):
-        st.text(question)
+        st.text(user_message)
 
     # Display assistant response as a speech bubble.
     with st.chat_message("assistant"):
@@ -360,17 +433,17 @@ if question := st.chat_input("Ask a question..."):
             if time_diff < MIN_TIME_BETWEEN_REQUESTS:
                 time.sleep(time_diff.seconds + time_diff.microseconds * 0.001)
 
-            question = question.replace("'", "")
+            user_message = user_message.replace("'", "")
 
         # Build a detailed prompt.
         if DEBUG_MODE:
             with st.status("Computing prompt...") as status:
-                full_prompt = build_question_prompt(question)
+                full_prompt = build_question_prompt(user_message)
                 st.code(full_prompt)
                 status.update(label="Prompt computed")
         else:
             with st.spinner("Researching..."):
-                full_prompt = build_question_prompt(question)
+                full_prompt = build_question_prompt(user_message)
 
         # Send prompt to LLM.
         with st.spinner("Thinking..."):
@@ -383,9 +456,9 @@ if question := st.chat_input("Ask a question..."):
             response = st.write_stream(response_gen)
 
             # Add messages to chat history.
-            st.session_state.messages.append({"role": "user", "content": question})
+            st.session_state.messages.append({"role": "user", "content": user_message})
             st.session_state.messages.append({"role": "assistant", "content": response})
 
             # Other stuff.
             show_feedback_controls(len(st.session_state.messages) - 1)
-            send_telemetry(question=question, response=response)
+            send_telemetry(question=user_message, response=response)
